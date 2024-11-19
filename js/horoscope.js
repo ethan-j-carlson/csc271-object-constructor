eea6799a82d41f8d0db2909de4f105b9301b4476
Ethan Carlson <98480330+ethan-j-carlson@users.noreply.github.com>
2024-11-19 18:10:22 +0000

    did the thing

--- js/horoscope.js
+++ js/horoscope.js
@@ -4,6 +4,20 @@ function displayTitle() {
 	contentDiv.innerHTML += title;
 };
 
+function Zodiac (cardNum, img, sign, birthday, horoscope) {
+	this.birthday = birthday
+	this.img = img
+	this.sign = sign
+	this.cardNum = cardNum
+	this.horoscope = horoscope
+	this.stars = function (count) {
+		res = ""
+		for (let i = 0; i < count; i++) {
+			res += "*"
+		}
+		return res;
+	};
+}
 
 function displayHoroscopeCard(cardNum, img, sign, birthday, horoscope, stars) {
 	var card = document.getElementsByClassName('card')[cardNum];
@@ -15,10 +29,16 @@ function displayHoroscopeCard(cardNum, img, sign, birthday, horoscope, stars) {
 	card.innerHTML += '<p><strong>Mood:</strong> ' + stars + '</p>';
 };
 
-
-
-
+let cap = new Zodiac(
+	0,
+	"capricorn.png",
+	"capricorn",
+	"*birthdays*",
+	"Today will be a day",
+);
 
 
 
 displayTitle();
+
+displayHoroscopeCard(cap.cardNum, cap.img, cap.sign, cap.birthday, cap.horoscope, cap.stars(3));
